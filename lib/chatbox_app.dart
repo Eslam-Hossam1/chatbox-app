ff0e27ab6ff8907285c7557e3642b42cde6d1a2f
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-23 01:43:46 +0300

    feat(core): setup theme

--- lib/chatbox_app.dart
+++ lib/chatbox_app.dart
@@ -1,13 +1,23 @@
+import 'package:chatbox_app/core/cubit/theme_cubit.dart';
 import 'package:chatbox_app/core/routing/app_router.dart';
+import 'package:chatbox_app/core/theme/app_themes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ChatboxApp extends StatelessWidget {
   const ChatboxApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
+    return BlocBuilder<ThemeCubit, ThemeMode>(
+      builder: (context, mode) {
+        return MaterialApp.router(
+          routerConfig: AppRouter.router,
+          themeMode: mode,
+          theme: AppThemes.lightTheme,
+          darkTheme: AppThemes.darkTheme,
+        );
+      },
     );
   }
 }
---
+++ lib/core/theme/app_themes.dart
@@ -0,0 +1,50 @@
+import 'package:chatbox_app/core/theme/app_colors.dart';
+import 'package:chatbox_app/core/theme/custom_colors.dart';
+import 'package:flutter/material.dart';
+
+abstract class AppThemes {
+  static final ThemeData lightTheme = ThemeData(
+    dialogBackgroundColor: AppColors.lightDialogBackground,
+    scaffoldBackgroundColor: AppColors.lightScaffoldBackground,
+    brightness: Brightness.light,
+    colorScheme: const ColorScheme.light(
+      primary: AppColors.lightPrimary,
+      secondary: AppColors.lightSecondary,
+      onSurfaceVariant: AppColors.lightOnSurfaceVariant,
+      outline: AppColors.lightOutline,
+    ),
+    extensions: <ThemeExtension<dynamic>>[
+      CustomColors(
+        mainTextColor: AppColors.lightMainTextColor,
+        secondaryTextColor: AppColors.lightSecondaryTextColor,
+        formColor: AppColors.lightFormColor,
+        toastColor: AppColors.lightToastBackground,
+        cachedNetworkImagePlaceholderColor:
+            AppColors.lightCachedNetworkImagePlacholderColor,
+      ),
+    ],
+  );
+
+  static final ThemeData darkTheme = ThemeData(
+    dialogBackgroundColor: AppColors.darkDialogBackground,
+    scaffoldBackgroundColor: AppColors.darkScaffoldBackground,
+    colorScheme: ColorScheme.dark(
+      primary: AppColors.darkPrimary,
+      secondary: AppColors.darkSecondary,
+      onSurfaceVariant: AppColors.darkOnSurfaceVariant,
+      surface: AppColors.darkSurface,
+      outline: AppColors.darkOutline,
+    ),
+    brightness: Brightness.dark,
+    extensions: <ThemeExtension<dynamic>>[
+      CustomColors(
+        mainTextColor: AppColors.darkMainTextColor,
+        secondaryTextColor: AppColors.darkSecondaryTextColor,
+        formColor: AppColors.darkFormColor,
+        toastColor: AppColors.darkToastBackground,
+        cachedNetworkImagePlaceholderColor:
+            AppColors.darkCachedNetworkImagePlacholderColor,
+      ),
+    ],
+  );
+}
