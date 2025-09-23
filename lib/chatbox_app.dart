1553ba96c4d6112476f41aca32e255d160e5dd6e
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-23 05:56:21 +0300

    feat: setup flutter screen util

--- lib/chatbox_app.dart
+++ lib/chatbox_app.dart
@@ -1,23 +1,37 @@
 import 'package:chatbox_app/core/cubit/theme_cubit.dart';
 import 'package:chatbox_app/core/routing/app_router.dart';
 import 'package:chatbox_app/core/theme/app_themes.dart';
+import 'package:chatbox_app/core/utils/size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ChatboxApp extends StatelessWidget {
   const ChatboxApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<ThemeCubit, ThemeMode>(
-      builder: (context, mode) {
-        return MaterialApp.router(
-          routerConfig: AppRouter.router,
-          themeMode: mode,
-          theme: AppThemes.lightTheme,
-          darkTheme: AppThemes.darkTheme,
-        );
-      },
+    return BlocProvider(
+      create: (context) => ThemeCubit(),
+      child: BlocBuilder<ThemeCubit, ThemeMode>(
+        builder: (context, mode) {
+          return ScreenUtilInit(
+            designSize: const Size(
+              SizeConfig.designWidth,
+              SizeConfig.designheight,
+            ),
+            minTextAdapt: true,
+            splitScreenMode: true,
+            child: MaterialApp.router(
+              debugShowCheckedModeBanner: false,
+              routerConfig: AppRouter.router,
+              themeMode: mode,
+              theme: AppThemes.lightTheme,
+              darkTheme: AppThemes.darkTheme,
+            ),
+          );
+        },
+      ),
     );
   }
 }
