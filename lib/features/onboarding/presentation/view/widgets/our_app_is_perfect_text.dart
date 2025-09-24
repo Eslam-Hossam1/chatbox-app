ab16bf2978c53f6a40cf44be9526c7938d4f8d9b
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-24 16:42:36 +0300

    feat(core): create OurAppIsPerfectText widget

---
+++ lib/features/onboarding/presentation/view/widgets/our_app_is_perfect_text.dart
@@ -0,0 +1,21 @@
+import 'package:chatbox_app/core/theme/app_text_styles.dart';
+import 'package:flutter/material.dart';
+
+class OurAppIsPerfectText extends StatelessWidget {
+  const OurAppIsPerfectText({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Text(
+        'Our chat app is the perfect way to stay\nconnected with friends and family.',
+        style: AppTextStyles.regular16(context, upperLimitRatio: 1.75).copyWith(
+          color: Color(0xffB9C1BE),
+          height: 26 / 16,
+        ),
+      ),
+    );
+  }
+}
