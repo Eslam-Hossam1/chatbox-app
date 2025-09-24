a2c3c0578fcc9624f748e1cd5ee5e35b98d02362
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-24 23:20:25 +0300

    feat(sign-up): assing SignUpView to signUp route

--- lib/core/routing/app_routes/auth_routes.dart
+++ lib/core/routing/app_routes/auth_routes.dart
@@ -1,15 +1,11 @@
 import 'package:chatbox_app/core/routing/routes_paths.dart';
-import 'package:flutter/material.dart';
+import 'package:chatbox_app/features/sign-up/presentation/view/sign_up_view.dart';
 import 'package:go_router/go_router.dart';
 
 class AuthRoutes {
   static GoRoute signUp = GoRoute(
     path: RoutePaths.signUp,
-    builder: (context, state) => Scaffold(
-      body: Center(
-        child: Text('Sign Up PlaceHolder'),
-      ),
-    ),
+    builder: (context, state) => SignUpView(),
   );
   static List<GoRoute> routes = [
     signUp,
