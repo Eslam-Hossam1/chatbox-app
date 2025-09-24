0c44c40a256d1ed2933b1cf5fbc2854453b8e2b8
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-24 18:33:21 +0300

    feat: create OnBoardingCacheService

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -1,10 +1,16 @@
 import 'package:chatbox_app/core/cache/shared_pref/shared_prefernce_helper.dart';
+import 'package:chatbox_app/core/services/onboarding_cache_service.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final getIt = GetIt.instance;
 Future<void> setupServiceLocator() async {
   await _setupCaching();
+  getIt.registerSingleton<OnBoardingCacheService>(
+    OnBoardingCacheService(
+      sharedPreferencesHelper: getIt<SharedPreferencesHelper>(),
+    ),
+  );
 }
 
 Future<void> _setupCaching() async {
