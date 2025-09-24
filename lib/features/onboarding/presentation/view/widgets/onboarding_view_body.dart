feaed90b41763f32434a95aab37e52f656e9d4d1
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-24 16:57:04 +0300

    refactor(onboarding): extract chatbox svg to widget

--- lib/features/onboarding/presentation/view/widgets/onboarding_view_body.dart
+++ lib/features/onboarding/presentation/view/widgets/onboarding_view_body.dart
@@ -1,13 +1,11 @@
-import 'package:chatbox_app/core/utils/assets.dart';
 import 'package:chatbox_app/core/widget/app_padding.dart';
 import 'package:chatbox_app/core/widget/sliver_height_space.dart';
 import 'package:chatbox_app/core/widget/sliver_sticky_footer.dart';
+import 'package:chatbox_app/features/onboarding/presentation/view/widgets/chat_box_svg.dart';
 import 'package:chatbox_app/features/onboarding/presentation/view/widgets/connects_friends_text_section.dart';
 import 'package:chatbox_app/features/onboarding/presentation/view/widgets/get_started_button.dart';
 import 'package:chatbox_app/features/onboarding/presentation/view/widgets/our_app_is_perfect_text.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/svg.dart';
 
 class OnboardingViewBody extends StatelessWidget {
   const OnboardingViewBody({
@@ -22,12 +20,7 @@ class OnboardingViewBody extends StatelessWidget {
           SliverHeightSpace(
             height: 20,
           ),
-          SliverToBoxAdapter(
-            child: SvgPicture.asset(
-              Assets.imagesSvgsOnboardingChatbox,
-              width: 100.w,
-            ),
-          ),
+          ChatBoxSvg(),
           SliverHeightSpace(
             height: 50,
           ),
