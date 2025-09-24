6dbe5cb24c5d7b6021086891e324d3ded4a53731
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-24 16:55:30 +0300

    feat(onboarding): create GetStartedButton widget

--- lib/features/onboarding/presentation/view/widgets/onboarding_view_body.dart
+++ lib/features/onboarding/presentation/view/widgets/onboarding_view_body.dart
@@ -1,7 +1,10 @@
 import 'package:chatbox_app/core/utils/assets.dart';
 import 'package:chatbox_app/core/widget/app_padding.dart';
 import 'package:chatbox_app/core/widget/sliver_height_space.dart';
+import 'package:chatbox_app/core/widget/sliver_sticky_footer.dart';
 import 'package:chatbox_app/features/onboarding/presentation/view/widgets/connects_friends_text_section.dart';
+import 'package:chatbox_app/features/onboarding/presentation/view/widgets/get_started_button.dart';
+import 'package:chatbox_app/features/onboarding/presentation/view/widgets/our_app_is_perfect_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
@@ -32,6 +35,15 @@ class OnboardingViewBody extends StatelessWidget {
           SliverHeightSpace(
             height: 16,
           ),
+          OurAppIsPerfectText(),
+          SliverHeightSpace(
+            height: 40,
+          ),
+          SliverStickyFooter(
+            children: [
+              GetStartedButton(),
+            ],
+          )
         ],
       ),
     );
