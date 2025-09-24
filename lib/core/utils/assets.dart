f596a68f6fa26aa51c8c867f79f53fdce6e43419
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-24 16:18:38 +0300

    feat(onboarding): add onboarding chatbox svg

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesPngsSplashImageAndroid12
+  /// assets/images/pngs/splash_image_android_12.png
+  static const String imagesPngsSplashImageAndroid12 = "assets/images/pngs/splash_image_android_12.png";
+
+  /// Assets for imagesPngsSplashImageDark
+  /// assets/images/pngs/splash_image_dark.png
+  static const String imagesPngsSplashImageDark = "assets/images/pngs/splash_image_dark.png";
+
+  /// Assets for imagesPngsSplashImageLight
+  /// assets/images/pngs/splash_image_light.png
+  static const String imagesPngsSplashImageLight = "assets/images/pngs/splash_image_light.png";
+
+  /// Assets for imagesSvgsOnboardingChatbox
+  /// assets/images/svgs/onboarding_chatbox.svg
+  static const String imagesSvgsOnboardingChatbox = "assets/images/svgs/onboarding_chatbox.svg";
+}
+
