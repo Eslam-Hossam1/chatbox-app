23296e429f1ffa53449e5d862af680160489be86
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-09-25 04:42:56 +0300

    feat(sign-up): create sign up header

--- lib/features/sign-up/presentation/view/widgets/sign_up_view_body.dart
+++ lib/features/sign-up/presentation/view/widgets/sign_up_view_body.dart
@@ -1,3 +1,6 @@
+import 'package:chatbox_app/core/widget/app_padding.dart';
+import 'package:chatbox_app/core/widget/sliver_auth_header_text.dart';
+import 'package:chatbox_app/core/widget/spacing/sliver_height_space.dart';
 import 'package:flutter/material.dart';
 
 class SignUpViewBody extends StatelessWidget {
@@ -5,6 +8,21 @@ class SignUpViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return AppPadding(
+      child: CustomScrollView(
+        slivers: [
+          SliverHeightSpace(
+            height: 80,
+          ),
+          SliverToBoxAdapter(
+            child: SliverAuthHeaderText(
+              title: 'Welcome to ChatBox',
+              subtitle:
+                  'Get chatting with friends and family today\nby signing up for our chat app!',
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
