4cdb91b634ef4aa64abca61b3c07cd302edb2437
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-10-03 02:26:01 +0300

    feat(sign-up): add BlocListener to SignUpView to handle states

--- lib/features/sign_up/presentation/view/sign_up_view.dart
+++ lib/features/sign_up/presentation/view/sign_up_view.dart
@@ -1,14 +1,30 @@
+import 'package:chatbox_app/core/helper/dialog_helper.dart';
+import 'package:chatbox_app/features/sign_up/presentation/manager/sign_up_cubit/sign_up_state.dart';
 import 'package:chatbox_app/features/sign_up/presentation/view/widgets/sign_up_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignUpView extends StatelessWidget {
   const SignUpView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-        child: SignUpViewBody(),
+    return BlocListener(
+      listener: (context, state) {
+        if (state is SignUpSuccess) {
+          //TODO: go to otp
+        }
+        if (state is SignUpFailure) {
+          DialogHelper.showErrorDialog(
+            context,
+            errorMessage: state.errMessage,
+          );
+        }
+      },
+      child: const Scaffold(
+        body: SafeArea(
+          child: SignUpViewBody(),
+        ),
       ),
     );
   }
