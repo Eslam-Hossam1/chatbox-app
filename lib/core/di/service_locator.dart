6eb5a2fa7ffdbca6f801247d1dfec40a794f024d
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-10-03 02:19:40 +0300

    feat(di): inject DioConsumer and provide SignUpCubit to SignUpView

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -1,5 +1,10 @@
 import 'package:chatbox_app/core/cache/shared_pref/shared_prefernce_helper.dart';
+import 'package:chatbox_app/core/networking/dio_consumer.dart';
 import 'package:chatbox_app/core/services/onboarding_cache_service.dart';
+import 'package:chatbox_app/features/sign_up/data/datasources/sign_up_remote_data_source_impl.dart';
+import 'package:chatbox_app/features/sign_up/data/repos/sign_up_repo_impl.dart';
+import 'package:chatbox_app/features/sign_up/domain/usecases/sign_up_usecase.dart';
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -9,6 +14,12 @@ Future<void> setupServiceLocator() async {
   getIt.registerSingleton<OnBoardingCacheService>(
     OnBoardingCacheService(
       sharedPreferencesHelper: getIt<SharedPreferencesHelper>(),
+    ),
+  );
+ 
+  getIt.registerSingleton<DioConsumer>(
+    DioConsumer(
+      dio: Dio(),
     ),
   );
 }
