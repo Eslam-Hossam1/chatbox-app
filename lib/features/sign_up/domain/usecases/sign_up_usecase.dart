83853e76e96a7f8b9e96a70cc77d054b0b2eaa0f
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-10-03 02:23:45 +0300

    fix(sign-up): add SignUpRepo as attribute to SignUpUsecase

--- lib/features/sign_up/domain/usecases/sign_up_usecase.dart
+++ lib/features/sign_up/domain/usecases/sign_up_usecase.dart
@@ -7,7 +7,7 @@ import 'package:dartz/dartz.dart';
 class SignUpUsecase implements Usecase<ApiFailure, void, SignUpParams> {
   final SignUpRepo _signUpRepo;
 
-  SignUpUsecase(this._signUpRepo);
+  SignUpUsecase({required SignUpRepo signUpRepo}) : _signUpRepo = signUpRepo;
 
   @override
   Future<Either<ApiFailure, void>> call(SignUpParams signUpParams) {
