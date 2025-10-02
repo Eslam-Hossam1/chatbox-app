02cbb92851edfa89f526a1e691b6fa41b05db2c4
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-10-02 07:03:12 +0300

    feat(sign-up): create SignUpParams class

---
+++ lib/features/sign_up/domain/params/sign_up_params.dart
@@ -0,0 +1,13 @@
+class SignUpParams {
+  final String displayName;
+  final String username;
+  final String email;
+  final String password;
+
+  SignUpParams({
+    required this.displayName,
+    required this.username,
+    required this.email,
+    required this.password,
+  });
+}
