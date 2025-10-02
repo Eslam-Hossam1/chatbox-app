a47ed49720a45d3aa654cf1d88aeab16882d0ce4
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-10-02 07:09:57 +0300

    fix(sign-up): fix SignUpRepo

--- lib/features/sign_up/domain/sign_up_repo.dart
+++ lib/features/sign_up/domain/sign_up_repo.dart
@@ -1,5 +1,7 @@
+import 'package:chatbox_app/core/errors/api_failure.dart';
 import 'package:chatbox_app/features/sign_up/domain/params/sign_up_params.dart';
+import 'package:dartz/dartz.dart';
 
 abstract interface class SignUpRepo {
-  Future<void> signUp(SignUpParams params);
+  Future<Either<ApiFailure, void>> signUp(SignUpParams params);
 }
