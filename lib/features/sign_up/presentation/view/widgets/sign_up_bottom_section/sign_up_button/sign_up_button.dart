5df748a6fe6c02a03b84568635e4f8f0d660f9c9
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-10-03 02:27:41 +0300

    refactor(sign-up): move sign up bottom widgets to sign_up_bottom folder

---
+++ lib/features/sign_up/presentation/view/widgets/sign_up_bottom_section/sign_up_button/sign_up_button.dart
@@ -0,0 +1,30 @@
+import 'package:chatbox_app/core/widget/custom_text_button.dart';
+import 'package:chatbox_app/features/sign_up/presentation/manager/sign_up_cubit/sign_up_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SignUpButton extends StatelessWidget {
+  const SignUpButton({
+    super.key,
+    required this.enableAutoValidation,
+    required this.formKey,
+  });
+  final GlobalKey<FormState> formKey;
+  final void Function() enableAutoValidation;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomTextButton(
+      width: double.infinity,
+      text: 'Sign Up',
+      onPressed: () {
+        if (formKey.currentState!.validate()) {
+          formKey.currentState!.save();
+          context.read<SignUpCubit>().signUp();
+        } else {
+          enableAutoValidation();
+        }
+      },
+    );
+  }
+}
---
+++ lib/features/sign_up/presentation/view/widgets/sign_up_bottom_section/sign_up_button/sign_up_button_builder.dart
@@ -0,0 +1,33 @@
+import 'package:chatbox_app/core/widget/custom_loading_button.dart';
+import 'package:chatbox_app/features/sign_up/presentation/manager/sign_up_cubit/sign_up_cubit.dart';
+import 'package:chatbox_app/features/sign_up/presentation/manager/sign_up_cubit/sign_up_state.dart';
+import 'package:chatbox_app/features/sign_up/presentation/view/widgets/sign_up_bottom_section/sign_up_button/sign_up_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SignUpButtonBuilder extends StatelessWidget {
+  const SignUpButtonBuilder({
+    super.key,
+    required this.formKey,
+    required this.enableAutoValidation,
+  });
+  final GlobalKey<FormState> formKey;
+  final void Function() enableAutoValidation;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<SignUpCubit, SignUpState>(
+      builder: (context, state) {
+        return AbsorbPointer(
+          absorbing: state is SignUpLoading,
+          child: state is SignUpLoading
+              ? const CustomLoadingButton()
+              : SignUpButton(
+                  enableAutoValidation: enableAutoValidation,
+                  formKey: formKey,
+                ),
+        );
+      },
+    );
+  }
+}
