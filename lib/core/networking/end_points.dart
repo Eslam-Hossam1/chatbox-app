babf2ebd02c531dd4fc2e99808b789247df71321
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-10-03 22:20:42 +0300

    feat(otp): add otp endpoints to Endpoints class

--- lib/core/networking/end_points.dart
+++ lib/core/networking/end_points.dart
@@ -2,4 +2,6 @@ abstract class EndPoints {
   static const String baseUrl =
       "https://chatbox-production-561c.up.railway.app";
   static const String signUp = "/api/register";
+  static const String verifyAuthOtp = "/api/verify-otp";
+  static const String sendSignUpOtp = "/api/send-otp";
 }
