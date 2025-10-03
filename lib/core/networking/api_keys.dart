75abbe4927013bc9f41b1691bccee6d91ac606e6
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-10-03 22:20:04 +0300

    feat(core): update api keys

--- lib/core/networking/api_keys.dart
+++ lib/core/networking/api_keys.dart
@@ -1,6 +1,7 @@
 abstract class ApiKeys {
   static const String email = "email";
-  static const String code = "code";
-  static const String accessToken = "accessToken";
+  static const String otp = "otp";
+  static const String accessToken = "token";
   static const String refreshToken = "refreshToken";
+  static const String data = "data";
 }
