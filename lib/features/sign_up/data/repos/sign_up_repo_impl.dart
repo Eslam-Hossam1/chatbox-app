9b5d3ca9d88a82e86dcecf2a6f4641502f6005be
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-10-03 05:34:18 +0300

    fix(sign-up): add try catch to SignUpRepoImpl

--- lib/features/sign_up/data/repos/sign_up_repo_impl.dart
+++ lib/features/sign_up/data/repos/sign_up_repo_impl.dart
@@ -1,9 +1,11 @@
 import 'package:chatbox_app/core/errors/api_failure.dart';
+import 'package:chatbox_app/core/errors/dio_api_failure.dart';
 import 'package:chatbox_app/features/sign_up/data/datasources/sign_up_remote_data_source.dart';
 import 'package:chatbox_app/features/sign_up/data/models/sign_up_request_body.dart';
 import 'package:chatbox_app/features/sign_up/domain/params/sign_up_params.dart';
 import 'package:chatbox_app/features/sign_up/domain/sign_up_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class SignUpRepoImpl implements SignUpRepo {
   final SignUpRemoteDataSource _signUpRemoteDataSource;
@@ -13,9 +15,22 @@ class SignUpRepoImpl implements SignUpRepo {
 
   @override
   Future<Either<ApiFailure, void>> signUp(
-      {required SignUpParams signUpParams}) {
-    return _signUpRemoteDataSource.signUp(
-      signUpRequestBody: SignUpRequestBody.fromParams(signUpParams),
-    );
+      {required SignUpParams signUpParams}) async {
+    try {
+      await _signUpRemoteDataSource.signUp(
+        signUpRequestBody: SignUpRequestBody.fromParams(signUpParams),
+      );
+      return Right(null);
+    } catch (e) {
+      if (e is DioException) {
+        return Left(DioApiFailure.fromDioException(e));
+      } else {
+        return Left(
+          DioApiFailure.unknownException(
+            unKnownExceptionMsg: e.toString(),
+          ),
+        );
+      }
+    }
   }
 }
