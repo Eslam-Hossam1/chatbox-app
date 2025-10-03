bd2aa130f454ff94d4eeb7d02b77e5fe49d902bb
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-10-03 05:35:07 +0300

    fix(sign-up): specify cubit used in bloc listener in SignUpView

--- lib/features/sign_up/presentation/view/sign_up_view.dart
+++ lib/features/sign_up/presentation/view/sign_up_view.dart
@@ -1,18 +1,21 @@
 import 'package:chatbox_app/core/helper/dialog_helper.dart';
+import 'package:chatbox_app/core/routing/routes_paths.dart';
+import 'package:chatbox_app/features/sign_up/presentation/manager/sign_up_cubit/sign_up_cubit.dart';
 import 'package:chatbox_app/features/sign_up/presentation/manager/sign_up_cubit/sign_up_state.dart';
 import 'package:chatbox_app/features/sign_up/presentation/view/widgets/sign_up_view_body.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
 class SignUpView extends StatelessWidget {
   const SignUpView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener(
+    return BlocListener<SignUpCubit, SignUpState>(
       listener: (context, state) {
         if (state is SignUpSuccess) {
-          //TODO: go to otp
+          context.go(RoutePaths.otp);
         }
         if (state is SignUpFailure) {
           DialogHelper.showErrorDialog(
