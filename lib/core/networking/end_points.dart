8bcb39b8b894926d0f0304d050e9af7abe84915e
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-10-03 05:26:56 +0300

    fix(sign-up): correct sign up endpoint

--- lib/core/networking/end_points.dart
+++ lib/core/networking/end_points.dart
@@ -1,5 +1,5 @@
 abstract class EndPoints {
   static const String baseUrl =
       "https://chatbox-production-561c.up.railway.app";
-  static const String signUp = "/register";
+  static const String signUp = "/api/register";
 }
