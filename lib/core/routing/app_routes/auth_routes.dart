86d42eb488f18ed5625a73c548dcf1d84e12d47f
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-10-05 02:37:02 +0300

    feat(forgot-password): create fogot password route and provide cubit

--- lib/core/routing/app_routes/auth_routes.dart
+++ lib/core/routing/app_routes/auth_routes.dart
@@ -1,4 +1,4 @@
-import '../../services/storage_services/secure_storage/secure_storage_service.dart';
+import 'package:chatbox_app/core/services/storage_services/secure_storage/secure_storage_service.dart';
 
 import '../../networking/dio_consumer.dart';
 import '../routes_paths.dart';
@@ -13,6 +13,16 @@ import '../../../features/login/data/repos/login_repo_impl.dart';
 import '../../../features/login/domain/usecases/login_usecase.dart';
 import '../../../features/login/presentation/manager/login_cubit/login_cubit.dart';
 import '../../../features/login/presentation/view/login_view.dart';
+import '../../../features/forgot_password/data/datasources/forgot_password_remote_data_source_impl.dart';
+import '../../../features/forgot_password/data/repos/forgot_password_repo_impl.dart';
+import '../../../features/forgot_password/domain/usecases/forgot_password_usecase.dart';
+import '../../../features/forgot_password/presentation/manager/forgot_password_cubit/forgot_password_cubit.dart';
+import '../../../features/forgot_password/presentation/view/forgot_password_view.dart';
+import '../../../features/reset_password/data/datasources/reset_password_remote_data_source_impl.dart';
+import '../../../features/reset_password/data/repos/reset_password_repo_impl.dart';
+import '../../../features/reset_password/domain/usecases/reset_password_usecase.dart';
+import '../../../features/reset_password/presentation/manager/reset_password_cubit/reset_password_cubit.dart';
+import '../../../features/reset_password/presentation/view/reset_password_view.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
@@ -50,8 +60,42 @@ class AuthRoutes {
     ),
   );
 
+  static GoRoute forgotPassword = GoRoute(
+    path: RoutePaths.forgotPassword,
+    builder: (context, state) => BlocProvider(
+      create: (context) => ForgotPasswordCubit(
+        forgotPasswordUseCase: ForgotPasswordUsecase(
+          forgotPasswordRepo: ForgotPasswordRepoImpl(
+            forgotPasswordRemoteDataSource: ForgotPasswordRemoteDataSourceImpl(
+              apiConsumer: getIt<DioConsumer>(),
+            ),
+          ),
+        ),
+      ),
+      child: ForgotPasswordView(),
+    ),
+  );
+
+  static GoRoute resetPassword = GoRoute(
+    path: RoutePaths.resetPassword,
+    builder: (context, state) => BlocProvider(
+      create: (context) => ResetPasswordCubit(
+        resetPasswordUseCase: ResetPasswordUsecase(
+          resetPasswordRepo: ResetPasswordRepoImpl(
+            resetPasswordRemoteDataSource: ResetPasswordRemoteDataSourceImpl(
+              apiConsumer: getIt<DioConsumer>(),
+            ),
+          ),
+        ),
+      ),
+      child: ResetPasswordView(),
+    ),
+  );
+
   static List<GoRoute> routes = [
     signUp,
     login,
+    forgotPassword,
+    resetPassword,
   ];
 }
--- lib/core/routing/routing_helper.dart
+++ lib/core/routing/routing_helper.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
-
-import '../../features/otp/domain/entities/otp_reason.dart';
 import 'routes_paths.dart';
+import '../../features/otp/domain/entities/otp_reason.dart';
 
 abstract class RoutingHelper {
   static pushOtp(BuildContext context, {required OtpReason otpReason}) {
     context.push(RoutePaths.otp, extra: otpReason);
+  }
+
+  static pushForgotPassword(BuildContext context) {
+    context.push(RoutePaths.forgotPassword);
   }
 }
