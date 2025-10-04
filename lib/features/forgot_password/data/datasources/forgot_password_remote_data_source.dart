dd37424fc77c75577eabb6f2e5f8a6ae937c63b1
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-10-05 02:34:19 +0300

    feat(forgot-password): create FogotPasswordDataSource and its implementations

---
+++ lib/features/forgot_password/data/datasources/forgot_password_remote_data_source.dart
@@ -0,0 +1,5 @@
+import '../models/forgot_password_request_body.dart';
+
+abstract interface class ForgotPasswordRemoteDataSource {
+  Future<void> forgotPassword({required ForgotPasswordRequestBody forgotPasswordRequestBody});
+}
