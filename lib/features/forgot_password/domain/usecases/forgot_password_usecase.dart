db6e8ef611d2990a0bd62e1dc5c23caa967c1aae
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-10-05 02:29:20 +0300

    feat(forgot-password): create FogotPasswordUsecase

---
+++ lib/features/forgot_password/domain/usecases/forgot_password_usecase.dart
@@ -0,0 +1,17 @@
+import '../../../../core/errors/api_failure.dart';
+import '../../../../core/usecases/usecase.dart';
+import '../params/forgot_password_params.dart';
+import '../repos/forgot_password_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class ForgotPasswordUsecase implements Usecase<ApiFailure, void, ForgotPasswordParams> {
+  final ForgotPasswordRepo _forgotPasswordRepo;
+
+  ForgotPasswordUsecase({required ForgotPasswordRepo forgotPasswordRepo})
+      : _forgotPasswordRepo = forgotPasswordRepo;
+
+  @override
+  Future<Either<ApiFailure, void>> call(ForgotPasswordParams forgotPasswordParams) {
+    return _forgotPasswordRepo.forgotPassword(forgotPasswordParams: forgotPasswordParams);
+  }
+}
