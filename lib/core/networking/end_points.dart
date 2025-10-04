f6be77d333674acf0c3d58fd75d9bd6f1cf40d7f
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-10-04 15:54:35 +0300

    feat(login): add login endpoint to EndPoints class

--- lib/core/networking/end_points.dart
+++ lib/core/networking/end_points.dart
@@ -2,6 +2,7 @@ abstract class EndPoints {
   static const String baseUrl =
       "https://chatbox-production-561c.up.railway.app";
   static const String signUp = "/api/register";
+  static const String login = "/api/login";
   static const String verifyAuthOtp = "/api/verify-otp";
   static const String sendSignUpOtp = "/api/send-otp";
 }
