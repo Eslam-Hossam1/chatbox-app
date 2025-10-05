a92814907b315aa0921c9af873dc21501ab908bb
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-10-05 05:47:47 +0300

    feat(reset-password): create ResetPasswordRepo

---
+++ lib/features/reset_password/domain/repos/reset_password_repo.dart
@@ -0,0 +1,9 @@
+import 'package:chatbox_app/features/reset_password/domain/params/reset_password_params.dart';
+
+import '../../../../../core/errors/api_failure.dart';
+import 'package:dartz/dartz.dart';
+
+abstract interface class ResetPasswordRepo {
+  Future<Either<ApiFailure, void>> resetPassword(
+      {required ResetPasswordParams resetPasswordParams});
+}
